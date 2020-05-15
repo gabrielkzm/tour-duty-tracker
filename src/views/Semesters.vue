46e22e5c0e0a0f5be2a6e2e6089009f23d5cd15b
Gabriel Koh <gabriel.koh.2016@sis.smu.edu.sg>
2020-05-15 15:10:24 +0800

    connected backend endpoint for Semesters view

--- src/views/Semesters.vue
+++ src/views/Semesters.vue
@@ -1,16 +1,18 @@
 <template>
   <v-container cols="12" fluid>
-    <v-row>
-      <BiteSizeCard title="Current Date Today" :value="new Date().toISOString().substr(0,10)" />
-      <BiteSizeCard title="Current Semester ID" :value="this.semesters[0]['semesterID']" />
-      <BiteSizeCard title="Current Semester Start Date" :value="this.semesters[0]['startDate']" />
-      <BiteSizeCard title="Current Semester End Date" :value="this.semesters[0]['endDate']" />
+    <v-row v-if="semesterCard !== null">
+      <BiteSizeCard title="Current Date Today" :value="today.toISOString().substr(0,10)" />
+      <BiteSizeCard title="Current Semester ID" :value="semesterCard.semesterID" />
+      <BiteSizeCard title="Current Semester Start Date" :value="semesterCard.startDate" />
+      <BiteSizeCard title="Current Semester End Date" :value="semesterCard.endDate" />
     </v-row>
     <v-row>
       <v-col>
         <v-data-table
           :headers="headers"
           :items="semesters"
+          :loading="loading"
+          :loading-text="loadingText"
           sort-desc="startDate"
           class="elevation-12"
         >
@@ -25,7 +27,7 @@
                 </template>
                 <SemesterForm
                   :semester="editedItem"
-                  :onCancel="close" 
+                  :onCancel="close"
                   :onSubmit="save"
                   :formTitle="formTitle"
                 />
@@ -39,23 +41,46 @@
         </v-data-table>
       </v-col>
     </v-row>
+    <v-snackbar color="success" :timeout="timeout" top v-model="snackbarSuccess">
+      {{snackbarText}}
+      <v-btn dark text @click="snackbarSuccess=false">Close</v-btn>
+    </v-snackbar>
+    <v-snackbar color="error" :timeout="timeout" top v-model="snackbarFail">
+      {{snackbarText}}
+      <v-btn dark text @click="snackbarFail=false">Close</v-btn>
+    </v-snackbar>
+    <v-snackbar color="warning" timeout="5000" right v-model="snackbarDelete">
+      <span>Are you sure you want to delete this semester?</span>
+      <v-btn dark text @click="deleteSemester">Delete</v-btn>
+      <v-btn dark text @click="snackbarDelete=false">Close</v-btn>
+    </v-snackbar>
   </v-container>
 </template>
 
 <script>
 import BiteSizeCard from "@/components/BiteSizeCard";
-import SemesterForm from '@/components/SemesterForm';
+import SemesterForm from "@/components/SemesterForm";
 
 export default {
   name: "Semesters",
 
   components: {
     BiteSizeCard,
-    SemesterForm,
+    SemesterForm
   },
 
   data: () => ({
+    index: null,
+    today: new Date(),
+    semesterCard: null,
+    loading: "#151c55",
+    loadingText: "Loading items...",
     dialog: false,
+    snackbarText: "",
+    snackbarSuccess: false,
+    snackbarFail: false,
+    snackbarDelete: false,
+    timeout: 2000,
     headers: [
       {
         text: "Semester ID",
@@ -68,6 +93,11 @@ export default {
       { text: "Actions", value: "actions", sortable: false }
     ],
     semesters: [],
+    viewItem: {
+      semesterID: 0,
+      startDate: null,
+      endDate: null,
+    },
     editedItem: {
       semesterID: 0,
       startDate: new Date().toISOString().substr(0, 10),
@@ -81,8 +111,10 @@ export default {
   }),
 
   computed: {
-    formTitle() {
-      return this.editedItem.semesterID === 0 ? "Add Semester" : "Edit Semester";
+    formTitle: function() {
+      return this.editedItem.semesterID === 0
+        ? "Add Semester"
+        : "Edit Semester";
     }
   },
 
@@ -92,75 +124,126 @@ export default {
     }
   },
 
-  created() {
-    this.initialize();
+  mounted() {
+    const todayTime = this.today.getTime();
+    this.$http
+      .get("semesters")
+      .then(response => {
+        response.data.semesters.map(semester => {
+          if (
+            todayTime >= new Date(semester.startDate).getTime() &&
+            todayTime <= new Date(semester.endDate).getTime()
+          ) {
+            this.semesterCard = {
+              semesterID: semester._id,
+              startDate: semester.startDate.substr(0, 10),
+              endDate: semester.endDate.substr(0, 10)
+            };
+          }
+          this.semesters.push({
+            semesterID: semester._id,
+            startDate: semester.startDate.substr(0, 10),
+            endDate: semester.endDate.substr(0, 10)
+          });
+        });
+        this.loading = false;
+      })
+      .catch(error => {
+        const message = "Loading failed. Please contact Tours Portfolio Head/Exco/Platform Adminstrator.";
+        this.snackbarFail = true;
+        this.snackbarText = message;
+        this.loadingText = message;
+        console.log(error);
+      });
   },
 
   methods: {
-    initialize() {
-      this.semesters = [
-        {
-          semesterID: 1,
-          startDate: new Date("March 20, 2021, 12:00:01")
-            .toISOString()
-            .substr(0, 10),
-          endDate: new Date("October 20, 2021, 12:00:01")
-            .toISOString()
-            .substr(0, 10)
-        },
-        {
-          semesterID: 2,
-          startDate: new Date("October 20, 2020, 12:00:01")
-            .toISOString()
-            .substr(0, 10),
-          endDate: new Date("January 20, 2021, 12:00:01")
-            .toISOString()
-            .substr(0, 10)
-        },
-        {
-          semesterID: 3,
-          startDate: new Date("January 10, 2020, 12:00:01")
-            .toISOString()
-            .substr(0, 10),
-          endDate: new Date("June 20, 2020, 12:00:01")
-            .toISOString()
-            .substr(0, 10)
-        }
-      ];
-    },
-
     editItem(item) {
+      this.index = this.semesters.indexOf(item);
       this.editedItem = Object.assign({}, item);
       this.dialog = true;
     },
 
     deleteItem(item) {
-      const index = this.semesters.indexOf(item);
-      confirm("Are you sure you want to delete this semester?") &&
-        this.semesters.splice(index, 1);
+      this.index = this.semesters.indexOf(item);
+      this.viewItem = Object.assign({}, item);
+      this.snackbarDelete = true;
+    },
+
+    deleteSemester() {
+      this.$http
+        .delete(`semesters/${this.viewItem.semesterID}`)
+        .then(response => {
+          this.snackbarText = response.data.message;
+          this.snackbarSuccess = true;
+          this.semesters.splice(this.index, 1);
+        })
+        .catch(error => {
+          this.snackbarText =
+            "Something went wrong. Please contact Tours Portfolio Head/EXCO/Administrator.";
+          this.snackbarFail = true;
+          console.log(error);
+        })
+        .then( () => {
+          this.snackbarDelete = false;
+          this.close();
+        })
     },
 
     close() {
       this.dialog = false;
       setTimeout(() => {
+        this.index = null;
         this.editedItem = Object.assign({}, this.defaultItem);
+        this.viewItem = Object.assign({}, this.defaultItem);
       }, 200);
     },
 
     save() {
       let editedItem = this.editedItem;
       if (editedItem.semesterID !== 0) {
-        for(let i= 0; i < this.semesters.length; i++){
-          let sem = this.semesters[i];
-          if (sem.semesterID === editedItem.semesterID){
-            Object.assign(this.semesters[i], this.editedItem);
-          }
-        }
+        this.$http
+          .put(`semesters/${editedItem.semesterID}`, editedItem)
+          .then(response => {
+            Object.assign(this.semesters[this.index], {
+              semesterID: response.data.semester._id,
+              startDate: response.data.semester.startDate.substr(0, 10),
+              endDate: response.data.semester.endDate.substr(0, 10)
+            });
+            this.snackbarText = response.data.message;
+            this.snackbarSuccess = true;
+          })
+          .catch(error => {
+            this.snackbarText =
+              "Something went wrong. Please contact Tours Portfolio Head/EXCO/Administrator.";
+            this.snackbarFail = true;
+            console.log(error);
+          })
+          .then(()=> {
+            this.close();
+          })
       } else {
-        editedItem.semesterID = Math.floor(Math.random() * Math.floor(100));
-        this.semesters.push(this.editedItem);
+        this.$http
+          .post("semesters", this.editedItem)
+          .then(response => {
+            this.semesters.push({
+              semesterID: response.data.semester._id,
+              startDate: response.data.semester.startDate.substr(0, 10),
+              endDate: response.data.semester.endDate.substr(0, 10)
+            });
+            this.snackbarText = response.data.message;
+            this.snackbarSuccess = true;
+          })
+          .catch(error => {
+            this.snackbarText =
+              "Something went wrong. Please contact Tours Portfolio Head/EXCO/Administrator.";
+            this.snackbarFail = true;
+            console.log(error);
+          })
+          .then(() => {
+            this.close();
+          });
       }
-      this.close();
     }
   }
 };
